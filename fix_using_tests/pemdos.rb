b46988879008c04f871a4e568238734a4f9d7294
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-08 17:55:07 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 
-def snake_it_up(string)
+def snake_it_up(string = "string")
   if string[0] == "s"
-    10 * "s" + string
+    "ssssssssss" + string
   else
     string
   end
